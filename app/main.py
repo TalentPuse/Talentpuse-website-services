eb10a47ea0970f6e76662041cc3a078288d2e11c
baominh5xx2 <baominh5xxx2@gmail.com>
2026-07-06 20:39:20 +0700

    fix(cors): allow PATCH method (job application status updates)

--- app/main.py
+++ app/main.py
@@ -85,7 +85,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
     allow_headers=["*"],
 )
 
